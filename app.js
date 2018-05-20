ecdda0b9701591b644df0c0187fb70b8765e1231
Enejo James <spec.en.james@gmail.com>
2018-05-20 06:16:48 +0100

    setup routes

---
+++ app.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import logger from 'morgan';
+
+const app = express();
+const port = parseInt(process.env.PORT, 10) || 8000;
+
+// Set folder to serve static files from
+app.use(express.static('static-files'));
+
+// Log to console
+app.use(logger('combined'));
+
+// Routes
+app.use('/', (req, res) => {
+    res.sendFile('index.html');
+});
+app.use('/thedream', (req, res) => {
+    res.sendFile('thedream.html');
+});
+app.use('/why', (req, res) => {
+    res.sendFile('why.html');
+});
+app.use('/about', (req, res) => {
+    res.sendFile('about.html');
+});
+
+app.listen(port);
